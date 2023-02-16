9ac5bb9841644edb5daf07325633411f5635a1a8
mdsorwar.mostafa <mdsorwar.mostafa@bracits.com>
2023-02-16 12:44:10 +0600

    combined rows and colums and experiment with allignments, arragements, weights

--- app/src/main/java/com/example/jetpackcomposesampleproject/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesampleproject/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.jetpackcomposesampleproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposesampleproject.ui.theme.JetpackComposeSampleProjectTheme
 import com.example.jetpackcomposesampleproject.ui.theme.Typography
 
@@ -20,15 +23,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeSampleProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column{
-                        Greeting("Android")
-                        CustomText(text = "Hello")
-                    }
+                Column(modifier = Modifier.fillMaxSize()) {
+
                 }
             }
         }
@@ -52,14 +48,124 @@ fun CustomText(text: String){
 @Composable
 fun DefaultPreview() {
     JetpackComposeSampleProjectTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            Column{
-                Greeting("Android")
-                CustomText(text = "Hello")
+        Row (modifier = Modifier.fillMaxSize()){
+            Column(
+                modifier = Modifier.fillMaxSize().weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.primary)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.secondary)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.primaryVariant)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.secondaryVariant)) {
+                }
+            }
+            Column(
+                modifier = Modifier.fillMaxSize().weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.primary)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.secondary)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.primaryVariant)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.secondaryVariant)) {
+                }
+            }
+            Column(
+                modifier = Modifier.fillMaxSize().weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.primary)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.secondary)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.primaryVariant)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(200.dp),
+                    color = (MaterialTheme.colors.secondaryVariant)) {
+                }
+            }
+            Column(
+                modifier = Modifier.fillMaxSize().weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .width(200.dp).weight(2f),
+                    color = (MaterialTheme.colors.primary)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .width(200.dp).weight(2f),
+                    color = (MaterialTheme.colors.secondary)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .width(200.dp).weight(2f),
+                    color = (MaterialTheme.colors.primaryVariant)) {
+                }
+                Surface(
+                    modifier = Modifier
+                        .width(200.dp).weight(1f),
+                    color = (MaterialTheme.colors.secondaryVariant)) {
+                }
             }
         }
+
     }
 }
