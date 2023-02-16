ab7e088d25b47ac8d6b322ee3f4b37ce73f56330
mdsorwar.mostafa <mdsorwar.mostafa@bracits.com>
2023-02-16 13:13:41 +0600

    use of custom scope to create reusable composable

--- app/src/main/java/com/example/jetpackcomposesampleproject/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesampleproject/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,6 +45,16 @@ fun CustomText(text: String){
    )
 }
 
+@Composable
+fun ColumnScope.CustomItem(weight: Float,color: Color = MaterialTheme.colors.primary){
+    Surface(
+        modifier = Modifier
+            .weight(weight)
+            .width(100.dp),
+        color = color) {
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -54,116 +65,8 @@ fun DefaultPreview() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.primary)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.secondary)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.primaryVariant)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.secondaryVariant)) {
-                }
-            }
-            Column(
-                modifier = Modifier.fillMaxSize().weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.primary)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.secondary)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.primaryVariant)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.secondaryVariant)) {
-                }
-            }
-            Column(
-                modifier = Modifier.fillMaxSize().weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
-            ) {
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.primary)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.secondary)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.primaryVariant)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(200.dp),
-                    color = (MaterialTheme.colors.secondaryVariant)) {
-                }
-            }
-            Column(
-                modifier = Modifier.fillMaxSize().weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Surface(
-                    modifier = Modifier
-                        .width(200.dp).weight(2f),
-                    color = (MaterialTheme.colors.primary)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .width(200.dp).weight(2f),
-                    color = (MaterialTheme.colors.secondary)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .width(200.dp).weight(2f),
-                    color = (MaterialTheme.colors.primaryVariant)) {
-                }
-                Surface(
-                    modifier = Modifier
-                        .width(200.dp).weight(1f),
-                    color = (MaterialTheme.colors.secondaryVariant)) {
-                }
+                CustomItem(1f,MaterialTheme.colors.secondary)
+                CustomItem(1f,MaterialTheme.colors.primary)
             }
         }
 
